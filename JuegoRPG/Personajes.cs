a79df7a0fed097f4920e0c9aeca5a1620b42a9df
AlexisAle <alexistuc@yahoo.com>
2020-07-08 13:57:17 -0300

    alexis ale

---
+++ JuegoRPG/Personajes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoRPG
+{
+    class Personaje
+    {
+        /*Caracteristicas
+        velocidad; //1 a 10
+        destreza; //1 a 5
+        fuerza; //1 a 10
+        Nivel; //1 a 10
+        Armadura; //1 a 10*/
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int Nivel;
+        private int Armadura;
+        /*Datos
+        Tipo;
+        Nombre;
+        Apodo;
+        Fecha de Nacimiento;
+        Edad; //entre 0 a 300
+        Salud://100*/
+        private string tipo;
+        private string nombre;
+        private string apodo;
+        private string fechaNacimiento;
+        private int edad;
+        private int salud;
+
+        //metodos setters y getters
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel1 { get => Nivel; set => Nivel = value; }
+        public int Armadura1 { get => Armadura; set => Armadura = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public string FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+    }
+}
---
+++ JuegoRPG/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JuegoRPG
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            string[] nombres = { "ale", "alvaro", "yeye", "enzo", "maxi"};
+            string[] nicks = { "papichulo", "dengue", "vieja", "moxi" };
+
+            int cantidadPersonajes = 2;
+            Personaje players;
+            List<Personaje> players = new List<Personaje>();
+            for(int i=0; i < cantidadPersonajes; ++i)
+            {
+
+                players = new Personaje(nombres[new Random().Next(0,4)], nicks[new Random().Next(0,4)],);
+            }
+             
+        }
+        /*
+         static int cantidad = 2;
+        static void Main(string[] args)
+        {
+            Personaje jugador;
+            List<Personaje> ListaDeJugadores = new List<Personaje>();
+            string[] Nombres = { "Celeste", "Gonzalo", "Fabio", "Alex", "Fede" };
+            string[] Apodos = { "Ntvg", "Sotaski", "Soto", "Juampy", "kiko" };
+        string[] nombres = {"Jhon Salchichon", "Josefo", "Lady Sonsa"};
+            string[] apodos = {"Degolador", "El Destructor", "El Elegido"};
+            for(int i = 0; i < cantidad; i++)
+            {
+                jugador = new Personaje(Nombres[new Random().Next(0, 4)], Apodos[new Random().Next(0, 4)], i+1);
+                ListaDeJugadores.Add(jugador);
+                jugador.MostrarPersonajes();
+            }
+            Combate(ListaDeJugadores);
+         */
+    }
+}
